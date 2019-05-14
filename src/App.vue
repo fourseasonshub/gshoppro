b5165203190f878ef781a415e82623ed969194e0
fourseasonshub <mangowang1212@163.com>
2019-05-14 16:20:33 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FootGuide v-if="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import FootGuide from './components/FootGuide/FootGuide.vue'
+
+  export default {
+    components:{
+      FootGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
